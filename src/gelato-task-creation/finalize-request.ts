6164759e3f67d36a9143ad3597e074fe03288228
0xRaccoon <raccoon@defi.sucks>
2023-11-29 15:41:15 +0100

    chore: replace gelato automation with relay
    Signed-off-by: 0xRaccoon <raccoon@defi.sucks>

--- src/gelato-task-creation/finalize-request.ts
+++ src/gelato-task-creation/finalize-request.ts
@@ -1,36 +1,29 @@
-import { AutomateSDK, TaskTransaction } from '@gelatonetwork/automate-sdk';
-import config from '../config/config';
 import hre from 'hardhat';
 import { abi as IOracleAbi } from '@defi-wonderland/prophet-core-abi/abi/IOracle.json';
 import { address } from '../constants';
 import { IOracle } from '@defi-wonderland/prophet-sdk/dist/src/types/typechain';
+import { GelatoRelay, RelayResponse, SponsoredCallRequest } from '@gelatonetwork/relay-sdk';
+import { ethers, ContractRunner } from 'ethers-v6';
 
 export class FinalizeRequest {
   public async automateTask(
     request: IOracle.RequestStruct,
     response: IOracle.ResponseStruct
-  ): Promise<TaskTransaction> {
+  ): Promise<RelayResponse> {
     const [signer] = await hre.ethers.getSigners();
-    const automate = new AutomateSDK(config.CHAIN_ID, signer);
+    const runner = signer as unknown as ContractRunner;
+    const relay = new GelatoRelay();
+    const oracleContract: ethers.Contract = new ethers.Contract(address.deployed.ORACLE, IOracleAbi, runner);;
+    const { data } = await oracleContract.finalize.populateTransaction(request, response);
+    const gelatoRequest: SponsoredCallRequest = {
+      chainId: BigInt(hre.network.config.chainId),
+      target: address.deployed.ORACLE,
+      data: data,
+    };
 
-    const oracleContract = new hre.ethers.Contract(address.deployed.ORACLE, IOracleAbi, signer);
+    const apiKey = process.env.GELATO_API_KEY;
+    const relayResponse = await relay.sponsoredCall(gelatoRequest, apiKey);
 
-    const method = 'finalize';
-    const functionData = [request, response];
-    const name = `Finalize Request: ${response.requestId}`;
-
-    const { taskId, tx }: TaskTransaction = await automate.createTask({
-      execAddress: oracleContract.address, 
-      // address _job, bytes calldata _jobData, address _feeRecipient
-      execSelector: oracleContract.interface.getSighash(method),
-      //execData?: string;  [address(job), work(uint256 _counter) [_counter], msg.sender]
-      execData: oracleContract.interface.encodeFunctionData(method, functionData),
-      execAbi: oracleContract.interface.format('json') as string,
-      name: name,
-      dedicatedMsgSender: false,
-      singleExec: true,
-    });
-
-    return { taskId, tx };
+    return relayResponse;
   }
 }
--- src/gelato-task-creation/resolve-dispute.ts
+++ src/gelato-task-creation/resolve-dispute.ts
@@ -1,38 +1,32 @@
-import { AutomateSDK, TaskTransaction } from '@gelatonetwork/automate-sdk';
-import config from '../config/config';
 import hre from 'hardhat';
 import { abi as IOracleAbi } from '@defi-wonderland/prophet-core-abi/abi/IOracle.json';
 import { address } from '../constants';
 import { IOracle } from '@defi-wonderland/prophet-sdk/dist/src/types/typechain';
 import { DisputeWithId } from '@defi-wonderland/prophet-sdk';
+import { GelatoRelay, RelayResponse, SponsoredCallRequest } from '@gelatonetwork/relay-sdk';
+import { ethers, ContractRunner } from 'ethers-v6';
 
 export class ResolveDispute {
   public async automateTask(
     request: IOracle.RequestStruct,
     response: IOracle.ResponseStruct,
     disputeWithId: DisputeWithId
-  ): Promise<TaskTransaction> {
+  ): Promise<RelayResponse> {
     const [signer] = await hre.ethers.getSigners();
-    const automate = new AutomateSDK(config.CHAIN_ID, signer);
+    const relay = new GelatoRelay();
+    const runner = signer as unknown as ContractRunner;
+    const oracleContract = new ethers.Contract(address.deployed.ORACLE, IOracleAbi, runner);
 
-    const oracleContract = new hre.ethers.Contract(address.deployed.ORACLE, IOracleAbi, signer);
+    const { data } = await oracleContract.resolveDispute.populateTransaction(request, response, disputeWithId.dispute);
+    const gelatoRequest: SponsoredCallRequest = {
+      chainId: BigInt(hre.network.config.chainId),
+      target: address.deployed.ORACLE,
+      data: data,
+    };
 
-    const { taskId, tx }: TaskTransaction = await automate.createTask({
-      execAddress: oracleContract.address, 
-      // address _job, bytes calldata _jobData, address _feeRecipient
-      execSelector: oracleContract.interface.getSighash('resolveDispute(bytes32)'),
-      //execData?: string;  [address(job), work(uint256 _counter) [_counter], msg.sender]
-      execData: oracleContract.interface.encodeFunctionData('resolveDispute', [
-        request,
-        response,
-        disputeWithId.dispute,
-      ]),
-      execAbi: oracleContract.interface.format('json') as string,
-      name: `Resolve Dispute ${disputeWithId.disputeId}`,
-      dedicatedMsgSender: false,
-      singleExec: true,
-    });
+    const apiKey = process.env.GELATO_API_KEY;
+    const relayResponse = await relay.sponsoredCall(gelatoRequest, apiKey);
 
-    return { taskId, tx };
+    return relayResponse;
   }
 }
